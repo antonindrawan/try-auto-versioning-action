b9d3b2e7bf72489f3bd109fc2d0929abcd7afddd
Anton Indrawan <anton.indrawan@gmail.com>
2021-04-19 14:24:18 +0200

    fix: implement auto tag bump and push

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,55 @@
 import * as core from '@actions/core'
-import simpleGit, {GitError, SimpleGit, TagResult} from 'simple-git';
-import { SemVer } from 'semver';
+import simpleGit, {PushResult, SimpleGit, TagResult} from 'simple-git'
+import {SemVer} from 'semver'
+import * as fs from 'fs'
+
+declare let process: {
+  env: {
+    GITHUB_EVENT_PATH: string
+  }
+}
 
 async function run(): Promise<void> {
   try {
+    const ev = JSON.parse(
+      fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
+    )
+
+    const prNum = ev.pull_request.number
+    core.info(`GITHUB_EVENT_PATH: ${process.env.GITHUB_EVENT_PATH}`)
+    core.info(`Content: ${JSON.stringify(ev)}`)
+    core.info(`PR number: ${prNum}`)
+  } catch (error) {
+    core.info(error)
+  }
+
+  try {
     const git: SimpleGit = simpleGit()
-    let version: SemVer;
+    let version: SemVer
     const tags: TagResult = await git.tags()
     if (tags.latest === undefined) {
-      version = new SemVer("0.0.1")
-      core.debug(version.format());
+      version = new SemVer('0.0.1')
+      core.info(version.format())
     } else {
       version = new SemVer(tags.latest)
       version.inc('patch')
     }
 
-    await git.addTag(version.format()).then((value: { name: string; }) => {
-      console.log("addTag result: " + value['name'])
+    // add the tag
+    try {
+      const value: {name: string} = await git.addTag(version.format())
+      core.info(`addTag result: ${value['name']}`)
 
       // push the tag
-      git.push('origin', value['name']).then((success) => {
-        console.log('tag successfully pushed');
-      }).catch((error: GitError) => {
-        console.log("push tag failed: " + error);
+      try {
+        const pushResult: PushResult = await git.push('origin', value['name'])
+        core.info(`tag successfully pushed to ${String(pushResult.repo)}`)
+      } catch (error) {
         core.setFailed(error.message)
-      });
-
-    }).catch((error : GitError) => {
-      console.log("addTag failed: " + error)
+      }
+    } catch (error) {
       core.setFailed(error.message)
-    })
-
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
