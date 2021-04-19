8b571546dc35602ebc4cefd15e4075b1be30c4cf
Anton Indrawan <anton.indrawan@gmail.com>
2021-04-19 13:50:27 +0200

    feat: implement auto tag bump and push

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
+import simpleGit, {GitError, SimpleGit, StatusResult, TagResult} from 'simple-git';
+import { SemVer } from 'semver';
+import { getUnpackedSettings } from 'node:http2';
 
 async function run(): Promise<void> {
   try {
@@ -10,7 +13,34 @@ async function run(): Promise<void> {
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
+    const git: SimpleGit = simpleGit()
+
     core.setOutput('time', new Date().toTimeString())
+
+    let version: SemVer;
+    const tags: TagResult = await git.tags()
+    if (tags.latest === undefined) {
+      version = new SemVer("0.0.1")
+      core.debug(version.format());
+    } else {
+      version = new SemVer(tags.latest)
+      version.inc('patch')
+      core.debug("latest tag: " + version.format());
+    }
+
+    await git.addTag(version.format()).then((value: { name: string; }): void => {
+      console.log("addTag result: " + value['name'])
+      git.push('origin', value['name']).then((success) => {
+        console.log('repo successfully pushed');
+      }).catch((error: GitError) => {
+        console.log('repo push failed: ' + error);
+      });
+
+    }).catch((error : GitError) : void => {
+
+    })
+    //console.log(status.isClean());
+
   } catch (error) {
     core.setFailed(error.message)
   }
